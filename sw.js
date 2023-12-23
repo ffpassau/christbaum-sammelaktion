7883d7f831ed61e1b7441cbf699879c5cef34e38
Herting-J <herting@fim.uni-passau.de>
2023-12-23 22:52:55 +0100

    Improve worker logging

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('address-map-cache').then(function(cache) {
-      return cache.addAll([     
+      return Promise.all([
         '/images/icon-192x192.png',
         '/images/icon-512x512.png',
-      ]);
+      ].map(url => {
+        return cache.add(url).catch(error => {
+          console.error(`Failed to cache ${url}: ${error}`);
+        });
+      }));
     })
   );
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
